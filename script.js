5730018fd328a518a7f5cbc761eed04923990713
tyg-4 <145789301+tyg-4@users.noreply.github.com>
2023-10-07 13:10:22 +0500

    Add files via upload
    new update

--- script.js
+++ script.js
@@ -1,39 +1,91 @@
 const inputEl = document.getElementById('example')
-const btn = document.querySelector('.todo-list__btn')
+const btnEl = document.querySelector('.todo-list__btn')
 const fieldEl = document.querySelector('.todo-list__field')
 
-const arr = [
+const todoList = [
     {
-        isDone: false,
-        text: 'asdasd',
-        id: 1
+        id: 1,
+        text: 'some text',
+        isDone: true
     }
 ]
 
-function onBtnClick() {
-    if(inputEl.value.length) {
+let id = 2
+
+function render() {
+    fieldEl.innerHTML = ''
+    for(let item of todoList){
+        const el = createHtmlElement(item)
+        fieldEl.appendChild(el)
+    }
+}
+
+function createHtmlElement(item)  {
         const divEl = document.createElement('div')
         divEl.classList.add('todo-list__item')
-        //divEl.innerText = inputEl.value
+        divEl.innerText = inputEl.value
         // console.log(divEl.value)
 
         const checkbox = document.createElement('input')  
-        checkbox.type = 'checkbox'    
+        checkbox.type = 'checkbox' 
+        if(item.idDone) {
+            checkbox.checked = true
+            divEl.classList.add('todo-list__item_done')
+        }   
 
         const text = document.createElement('p')  
         text.classList.add('todo-list__item-text')
-        text.interText = inputEL.value
+        text.interText = item.text
 
         const img = document.createElement('img')
         img.src ='/img/trash.svg'
+
+        img.addEventListener('click', () => {
+            removeItem(item.id)
+        })
 
         divEl.appendChild(checkbox)
         divEl.appendChild(text)
         divEl.appendChild(img)
 
-        fieldEl.appendChild(divEl)
+        //fieldEl.appendChild(divEl)
+
+        //checkbox.addEventListener('change',() => {
+           // item.isDone = !item.isDone
+           // render()
+            //console.log(todoList)
+      //  }
+       // )
+
+        return divEl
+}
+
+function updateItem(id) {
+    const item = todoList.find((id) => i.id === id)
+    item.isDone = !item.isDone
+    render()
+} 
+
+function removeItem(id) {
+    const idx = todoList.findIndex((i) => i.id === id)
+    todoList.splice(idx, 1)
+    render()
+}
+
+function addItem() {
+    if (inputEl.value) {
+        const obj = {
+            text: inputEl.value,
+            isDone: false,
+            id: id++ 
+        }
+        todoList.push(obj)
+        //console.log(todoList)
         inputEl.value = ''
+        render()
     }
 }
 
-bhtEL.addEventListener('click', onBtnClick)
+btnEl.addEventListener('click', addItem)
+
+render()
